c10ca9880ba03063d5e026d91d23c79b665e3d3f
Alice Cadillon <acadillon@gmail.com>
2024-09-24 13:40:34 +0200

    modif de config>env>production>server.js en suivatn les recommandantion de CHat pour trouver le chemin du repertoire public

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,6 +1,8 @@
+const path = require('path');
+
 module.exports = ({ env }) => ({
   url: env("RENDER_EXTERNAL_URL"),
   dirs: {
-    public: "/public"
+    public: path.join(__dirname, '..', '..', 'public'), // Ajustez le chemin si nécessaire
   },
 });
