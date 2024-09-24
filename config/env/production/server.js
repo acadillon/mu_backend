0c4233da10891400abd396f1931b572e3afa4b8a
Alice Cadillon <acadillon@gmail.com>
2024-09-24 13:47:35 +0200

    Ajout de NODE_ENV=production dans .env

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -3,6 +3,6 @@ const path = require('path');
 module.exports = ({ env }) => ({
   url: env("RENDER_EXTERNAL_URL"),
   dirs: {
-    public: path.join(__dirname, '..', '..', 'public'), // Ajustez le chemin si nécessaire
+    public: "public"
   },
 });
